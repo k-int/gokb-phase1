06528bf031398228b2fa580764acdbe92f55dcd0
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-07-01 19:29:48 +0100

    Modify ValidateData command to create a Notification stack from the data returned from the server.

--- refine/extensions/gokb/src/com/k_int/gokb/refine/commands/ValidateData.java
+++ refine/extensions/gokb/src/com/k_int/gokb/refine/commands/ValidateData.java
@@ -8,15 +8,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.refine.ProjectManager;
+import com.google.refine.model.Project;
 import com.k_int.gokb.refine.A_RefineAPIBridge;
 import com.k_int.gokb.refine.RefineAPICallback;
 import com.k_int.gokb.refine.RefineUtil;
-
-import com.google.refine.ProjectManager;
-import com.google.refine.model.Project;
+import com.k_int.gokb.refine.ValidationMessage;
+import com.k_int.gokb.refine.notifications.NotificationStack;
 
 
 public class ValidateData extends A_RefineAPIBridge {
@@ -55,11 +58,36 @@ public class ValidateData extends A_RefineAPIBridge {
             postToAPI(response, "projectDataValid", params(request), files, new RefineAPICallback(){
 
                 @Override
-                protected void onSuccess(InputStream result, int responseCode)
-                        throws Exception {
-
+                protected void onSuccess(InputStream result, int responseCode) throws Exception {
+                  
+                    // Add messages to the correct stack.
+                    NotificationStack stack = NotificationStack.get("validation");
+                    
+                    // Clear the data as validation messages are always preserved.
+                    stack.clear();
+                    
+                    // The data.
+                    JSONObject data = getJSONObjectFromStream(result);
+                    
+                    // Grab the messages.
+                    JSONArray messages = data
+                      .getJSONObject("result")
+                      .getJSONArray("messages")
+                    ;
+                    
+                    // Go through each message and try and push a notification for each message.
+                    for (int i=0; i<messages.length(); i++) {
+                      ValidationMessage n = ValidationMessage.fromJSON(messages.getJSONObject(i).toString(), ValidationMessage.class);
+                      
+                      // We need to make sure it doesn't automatically hide itself.
+                      n.setHide(false);
+                      
+                      // Add to the stack.
+                      stack.add(n);
+                    }
+                    
                     // Proxy through the api response to the client.
-                    proxyReturn (response, result);
+                    respond (response, data.toString());
                 }
             });
 
