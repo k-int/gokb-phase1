a90dce9f96a834b084987db18abb99e3fc8a7914
Ian Ibbotson <ian.ibbotson@k-int.com>
2016-06-02 09:51:15 +0100

    updated expunge to handle title history event participant

--- server/gokb/grails-app/domain/org/gokb/cred/KBComponent.groovy
+++ server/gokb/grails-app/domain/org/gokb/cred/KBComponent.groovy
@@ -1092,6 +1092,7 @@ abstract class KBComponent {
     KBComponentVariantName.executeUpdate("delete from KBComponentVariantName as c where c.owner=:component",[component:this]);
 
     ReviewRequestAllocationLog.executeUpdate("delete from ReviewRequestAllocationLog as c where c.rr in ( select r from ReviewRequest as r where r.componentToReview=:component)",[component:this]);
+    ComponentHistoryEventParticipant.executeUpdate("delete from ComponentHistoryEventParticipant as c where c.participant = :component",[component:this]);
 
     ReviewRequest.executeUpdate("delete from ReviewRequest as c where c.componentToReview=:component",[component:this]);
     ComponentPerson.executeUpdate("delete from ComponentPerson as c where c.component=:component",[component:this]);
